c77a7e48521f33ff8a22f7f3f3d359ab598636f6
K4T <hesperis2@gmail.com>
2014-11-17 14:08:02 +0100

    Play command: full implementation.

--- src/Network/AMCP/Commands/Play.js
+++ src/Network/AMCP/Commands/Play.js
@@ -1,5 +1,34 @@
-var Play = function(channel, layer, clip) {
-    return 'PLAY ' + channel + '-' + layer + ' \"' + clip + '\"';
+var Play = function() {
+
+    var command = 'PLAY ';
+
+    var channel = arguments[0],
+        layer = (arguments[1] || 0);
+
+    switch (arguments.length) {
+        case 1:
+        case 2:
+            command += channel + '-' + layer;
+            break;
+
+        case 3:
+            var clip = arguments[2];
+            
+            command += channel + '-' + layer + ' \"' + clip + '\"';
+            break;
+
+        case 4:
+            var clip = arguments[2],
+                loadBGparams = arguments[3];
+
+                command += channel + '-' + layer + ' \"' + clip + '\" ' + loadBGparams;
+            break;
+
+        default:
+        // throw new Error('PLAY command: wrong parameters.');
+    }
+
+    return command;
 };
 
 module.exports = Play;
