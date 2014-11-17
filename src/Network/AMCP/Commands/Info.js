9b1f836b27012df23074f3f3f33a3ee051404e7c
K4T <hesperis2@gmail.com>
2014-11-17 12:08:05 +0100

    Info command: full implementation.

--- src/Network/AMCP/Commands/Info.js
+++ src/Network/AMCP/Commands/Info.js
@@ -1,11 +1,41 @@
-var Info = function(channel, layer) {
+var Info = function() {
 
     var command = 'INFO ';
 
-    command += channel;
+    switch (arguments.length) {
+        case 0:
+            command = command.trim();
+            break;
 
-    if (typeof layer !== 'undefined') {
-        command += '-'+layer
+        case 1:
+            command += arguments[0];
+            break;
+
+        case 2:
+            if (arguments[0] === 'TEMPLATE') {
+                var templateName = arguments[1];
+                command += arguments[0] + ' ' + templateName;
+            } else {
+                var channel = arguments[0];
+
+                if (arguments[1] === 'DELAY') {
+                    command += channel + ' ' + arguments[1];
+                } else {
+                   var layer = arguments[1];
+                    command += channel + '-' + layer;
+                }
+            }
+            break;
+
+        case 3:
+            var channel = arguments[0],
+                layer = arguments[1];
+
+                command += channel + '-' + layer + ' ' + arguments[2];
+            break;
+
+        default:
+            // throw new Error('INFO command: wrong parameters.');
     }
 
     return command;
